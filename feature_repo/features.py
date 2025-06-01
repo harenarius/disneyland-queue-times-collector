6234a3ffbbedbc721b60c0274947a4ae3f227455
Justin Rawlinson <justin.rawlinson@pm.me>
2025-06-01 14:27:24 -0700

    style: converge Black & isort on features.py

--- feature_repo/features.py
+++ feature_repo/features.py
@@ -1,10 +1,11 @@
+from datetime import timedelta
+
 from feast import Entity, FeatureView, Field, FileSource
 from feast.types import Float32, Int32
-from datetime import timedelta
 
 QUEUE_RAW = FileSource(
     name="queue_raw",
-    path="../data/raw/**/*.parquet",    # <- recursive to part files
+    path="../data/raw/**/*.parquet",  # <- recursive to part files
     timestamp_field="timestamp",
 )
 
@@ -21,10 +22,10 @@ queue_weather_hourly = FeatureView(
     entities=[ride],
     ttl=timedelta(days=90),
     schema=[
-        Field("posted_wait",  Int32),
-        Field("temp_f",       Float32),
-        Field("precip_prob",  Int32),
+        Field("posted_wait", dtype=Int32),
+        Field("temp_f", dtype=Float32),
+        Field("precip_prob", dtype=Int32),
     ],
     online=False,
-    source=QUEUE_RAW,                     # minimal MVP
+    source=QUEUE_RAW,  # minimal MVP
 )
